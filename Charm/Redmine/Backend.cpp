ed89ef320e5869fae6628fc1103a0b3439bee691
Mirko Boehm (Endocode) <mirko@endocode.com>
2014-05-28 09:35:29 +0200

    Complete storag skeleton.

--- Charm/Redmine/Backend.cpp
+++ Charm/Redmine/Backend.cpp
@@ -13,4 +13,225 @@ QString Backend::description() const
     return tr("Redmine Server");
 }
 
+void Backend::stateChanged(State previous)
+{
+    //NI
+}
+
+bool Backend::connect(Configuration &)
+{
+    //NI
+    return false;
+}
+
+bool Backend::disconnect()
+{
+    //NI
+    return false;
+}
+
+int Backend::installationId() const
+{
+    //NI
+    return false;
+}
+
+Installation Backend::getInstallation(int installationId)
+{
+    //NI
+    return Installation();
+}
+
+Installation Backend::createInstallation(const QString &name)
+{
+    //NI
+    return Installation();
+}
+
+bool Backend::modifyInstallation(const Installation &)
+{
+    //NI
+    return false;
+}
+
+bool Backend::deleteInstallation(const Installation &)
+{
+    //NI
+    return false;
+}
+
+User Backend::getUser(int userid)
+{
+    //NI
+    return User();
+}
+
+User Backend::makeUser(const QString &name)
+{
+    //NI
+    return User();
+}
+
+bool Backend::modifyUser(const User &user)
+{
+    //NI
+    return false;
+}
+
+bool Backend::deleteUser(const User &user)
+{
+    //NI
+    return false;
+}
+
+TaskList Backend::getAllTasks()
+{
+    //NI
+    return TaskList();
+}
+
+bool Backend::setAllTasks(const User &user, const TaskList &tasks)
+{
+    //NI
+    return false;
+}
+
+bool Backend::addTask(const Task &task)
+{
+    //NI
+    return false;
+}
+
+bool Backend::addTask(const Task &task, const SqlRaiiTransactor &)
+{
+    //NI
+    return false;
+}
+
+Task Backend::getTask(int taskId)
+{
+    //NI
+    return Task();
+}
+
+bool Backend::modifyTask(const Task &task)
+{
+    //NI
+    return false;
+}
+
+bool Backend::deleteTask(const Task &task)
+{
+    //NI
+    return false;
+}
+
+bool Backend::deleteAllTasks()
+{
+    //NI
+    return false;
+}
+
+bool Backend::deleteAllTasks(const SqlRaiiTransactor &)
+{
+    //NI
+    return false;
+}
+
+EventList Backend::getAllEvents()
+{
+    //NI
+    return EventList();
+}
+
+Event Backend::makeEvent()
+{
+    //NI
+    return Event();
+}
+
+Event Backend::makeEvent(const SqlRaiiTransactor &)
+{
+    //NI
+    return Event();
+}
+
+Event Backend::getEvent(int eventId)
+{
+    //NI
+    return Event();
+}
+
+bool Backend::modifyEvent(const Event &event)
+{
+    //NI
+    return false;
+}
+
+bool Backend::modifyEvent(const Event &event, const SqlRaiiTransactor &)
+{
+    //NI
+    return false;
+}
+
+bool Backend::deleteEvent(const Event &event)
+{
+    //NI
+    return false;
+}
+
+bool Backend::deleteAllEvents()
+{
+    //NI
+    return false;
+}
+
+bool Backend::deleteAllEvents(const SqlRaiiTransactor &)
+{
+    //NI
+    return false;
+}
+
+bool Backend::addSubscription(User, Task)
+{
+    //NI
+    return false;
+}
+
+bool Backend::deleteSubscription(User, Task)
+{
+    //NI
+    return false;
+}
+
+bool Backend::setMetaData(const QString &key, const QString &value)
+{
+    //NI
+    return false;
+}
+
+QString Backend::getMetaData(const QString &key)
+{
+    //NI
+    return tr("NI");
+}
+
+QString Backend::setAllTasksAndEvents(const User &, const TaskList &, const EventList &)
+{
+    //NI
+    return tr("NI");
+}
+
+bool Backend::createDatabase(Configuration &)
+{
+    //NI
+    return false;
+}
+
+bool Backend::verifyDatabase()
+{
+    //NI
+    return false;
+}
+
 }
--- Charm/Redmine/Backend.h
+++ Charm/Redmine/Backend.h
@@ -2,6 +2,17 @@
 #define BACKEND_H
 
 #include <QCoreApplication>
+#include <QString>
+
+#include "Core/Task.h"
+#include "Core/User.h"
+#include "Core/State.h"
+#include "Core/Event.h"
+#include "Core/Installation.h"
+
+class Event;
+class Configuration;
+class SqlRaiiTransactor;
 
 #include "Core/StorageInterface.h"
 
@@ -13,7 +24,45 @@ class Backend : public StorageInterface
 public:
     Backend();
     QString description() const override;
+    void stateChanged(State previous) override;
+    bool connect(Configuration&) override;
+    bool disconnect() override;
+    int installationId() const override;
+    Installation getInstallation(int installationId) override;
+    Installation createInstallation(const QString& name) override;
+    bool modifyInstallation(const Installation&) override;
+    bool deleteInstallation(const Installation&) override;
+    User getUser(int userid) override;
+    User makeUser(const QString& name) override;
+    bool modifyUser(const User& user) override;
+    bool deleteUser(const User& user) override;
+    TaskList getAllTasks() override;
+    bool setAllTasks( const User& user, const TaskList& tasks ) override;
+    bool addTask(const Task& task) override;
+    bool addTask( const Task& task, const SqlRaiiTransactor& ) override;
+    Task getTask(int taskId) override;
+    bool modifyTask(const Task& task) override;
+    bool deleteTask(const Task& task) override;
+    bool deleteAllTasks() override;
+    bool deleteAllTasks( const SqlRaiiTransactor& ) override;
+    EventList getAllEvents() override;
+    Event makeEvent() override;
+    Event makeEvent( const SqlRaiiTransactor& ) override;
+    Event getEvent(int eventId)override;
+    bool modifyEvent( const Event& event ) override;
+    bool modifyEvent( const Event& event, const SqlRaiiTransactor& ) override;
+    bool deleteEvent(const Event& event) override;
+    bool deleteAllEvents() override;
+    bool deleteAllEvents( const SqlRaiiTransactor& ) override;
+    bool addSubscription(User, Task) override;
+    bool deleteSubscription(User, Task) override;
+    bool setMetaData(const QString& key, const QString& value) override;
+    QString getMetaData(const QString& key) override;
+    QString setAllTasksAndEvents( const User&, const TaskList&, const EventList& ) override;
 
+protected:
+    bool createDatabase(Configuration&) override;
+    bool verifyDatabase() override;
 };
 
 }
